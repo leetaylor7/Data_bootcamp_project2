e6e165ccfc772d5a4f831f6ff754f58ae4b56874
leetaylor7 <ltaylor@goodmanfinancial.com>
2019-02-26 21:33:28 -0600

    started the flask

---
+++ app.py
@@ -0,0 +1,81 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+# Chris to look into why we get errors when not using the check_same_thread arg
+MySQL_root_PW = 'INSERT PASSWORD HERE'
+MySQL_db = 'gfc_data'
+engine = create_engine("mysql://root:"+MySQL_root_PW+"@localhost/"+MySQL_db)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Capacity_Utilization = Base.classes.passenger
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/names<br/>"
+        f"/api/v1.0/passengers"
+    )
+
+
+@app.route("/api/v1.0/names")
+def names():
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(Passenger.name).all()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names)
+
+
+@app.route("/api/v1.0/passengers")
+def passengers():
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Passenger).all()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_passengers = []
+    for passenger in results:
+        passenger_dict = {}
+        passenger_dict["name"] = passenger.name
+        passenger_dict["age"] = passenger.age
+        passenger_dict["sex"] = passenger.sex
+        all_passengers.append(passenger_dict)
+
+    return jsonify(all_passengers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
