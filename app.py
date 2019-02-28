7d538581944a3b53bdfb06ff0be9df144c85429d
Lee Taylor <ltaylor@goodmanfinancial.com>
2019-02-28 12:54:15 -0600

    Commit before more work is done

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 # import necessary libraries
 from flask import Flask, render_template
 
+#Imports all dependencies
+import pandas as pd
+import datetime
+import json
+import requests
+from pprint import pprint
+from sqlalchemy import create_engine, inspect, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, Date 
+import pymysql
+pymysql.install_as_MySQLdb()
+Base = declarative_base()
+import sqlite3
+from sqlalchemy.orm import Session
+
 # create instance of Flask app
 app = Flask(__name__)
 
@@ -19,7 +34,28 @@ def CoreListReports():
 @app.route('/supportfilesecon')
 def econsupportfiles():
 
-    return render_template('supportfilesecon.html')
+    #Creates the connection to the SQL File
+    conn = sqlite3.connect('gfc_data.sqlite')
+    engine = create_engine('sqlite:///gfc_data.sqlite')
+    session = Session(engine)
+
+    #Shows the class ids for all classes
+    class_id = ['econ_unemployment', "econ_new_orders", 'econ_non_defense_x_air', 'econ_capacity_utilization', 'econ_data_initial_claims', 'econ_data_weekly_earnings']
+
+    #Creates the dataframe for all data showing the last 10 observations
+    df = pd.read_sql("SELECT * FROM " + class_id[4]+ " ORDER BY id DESC LIMIT 10", conn)
+    df['Unemployment Date'] = pd.read_sql("SELECT date FROM " + class_id[0] + " ORDER BY id DESC LIMIT 10", conn)
+    df['Unemployment Rate'] = pd.read_sql("SELECT Unemployment_Rate FROM " + class_id[0]+ " ORDER BY id DESC LIMIT 10", conn)
+    df['New orders Date'] = pd.read_sql("SELECT date FROM " + class_id[1]+ " ORDER BY id DESC LIMIT 10", conn)
+    df['New Capital Goods Orders'] = pd.read_sql("SELECT Value_of_Manufactuters_New_Orders FROM " + class_id[1]+ " ORDER BY id DESC LIMIT 10", conn)
+    df['Cap goods Date'] = pd.read_sql("SELECT date FROM " + class_id[2] + " ORDER BY id DESC LIMIT 10", conn)
+    df['Capital Goods x Defense Aircraft New Orders'] = pd.read_sql("SELECT Manufacturers_New_Orders_Non_Defense_Cap_Goods_x_Air FROM " + class_id[2] + " ORDER BY id DESC LIMIT 10", conn)
+    df['utilization Date'] = pd.read_sql("SELECT date FROM " + class_id[3] + " ORDER BY id DESC LIMIT 10", conn)
+    df['factory utilization rate'] = pd.read_sql("SELECT Capacity_Utilization_Manufacturing FROM " + class_id[3] + " ORDER BY id DESC LIMIT 10", conn)
+    df['Weekly Earnings Date'] = pd.read_sql("SELECT date FROM " + class_id[5] + " ORDER BY id DESC LIMIT 10", conn)
+    df['Average Weekly Earnings'] = pd.read_sql("SELECT Average_Weekly_Earnings FROM " + class_id[5] + " ORDER BY id DESC LIMIT 10", conn)
+
+    return render_template('supportfilesecon.html', data=df.to_html())
 
 
 if __name__ == "__main__":
